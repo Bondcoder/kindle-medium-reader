1991e7eb90062613b8c0694742fd1ed605534d07
Patricia Gonzalez <aloha@bondcoder.eu>
2020-08-03 08:01:42 +0100

    The server is rejecting the connection (needs to be fixed)

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ async function openBrowser() {
 
 async function fetchArticle(url, number) {
     let page = await browser.newPage();
-    await page.setViewport({width: 1200, height: 800});
+    await page.setViewport({ width: 1200, height: 800 });
     await page.goto(url, { waitUntil: "load" });
-    if (number=== 0) {
+    if (number === 0) {
         await page.$eval('body > div:nth-child(19) > div > div > div > div > div > div > span > button', el => el.click());
     }
     await autoScroll(page);
-    await page.pdf({ 
+    await page.pdf({
         path: "PDF/article-" + number + ".pdf",
         format: "A4",
         margin: {
@@ -23,25 +23,21 @@ async function fetchArticle(url, number) {
             bottom: 60,
             left: 60,
             right: 50
-          },
+        },
         printBackground: true
     })
 }
 
-async function readTextFile()
-{
+async function readTextFile() {
     let startTime = Date.now();
     var file = "file://C:/Users/pgonzalez/Documents/medium-kindle-reader/ToRead/articles.txt";
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
                 articles = rawFile.responseText;
-                articles = articles.replace(/(\r\n|\n|\r)/gm," ").split(' ');
+                articles = articles.replace(/(\r\n|\n|\r)/gm, " ").split(' ');
             }
         }
     }
@@ -65,14 +61,14 @@ async function mergePdfs() {
     const PDFMerger = require('pdf-merger-js');
     var merger = new PDFMerger();
     var files = getFiles()
-    for(let i = 0; i < files.length; i++) {
+    for (let i = 0; i < files.length; i++) {
         merger.add('./PDF/' + files[i])
     }
     let today = new Date().toISOString().slice(0, 10)
     await merger.save(today + '.pdf')
 }
 
-async function autoScroll(page){
+async function autoScroll(page) {
     await page.evaluate(async () => {
         await new Promise((resolve, reject) => {
             var totalHeight = 0;
@@ -82,7 +78,7 @@ async function autoScroll(page){
                 window.scrollBy(0, distance);
                 totalHeight += distance;
 
-                if(totalHeight >= scrollHeight){
+                if (totalHeight >= scrollHeight) {
                     clearInterval(timer);
                     resolve();
                 }
